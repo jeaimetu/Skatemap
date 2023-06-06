81237a967705e5b1ef20967c7e506b8e75112bf1
teddy <jeaimetu@naver.com>
2023-06-06 15:10:52 +0900

    naver map연결안되는 것 수정함. custom webview client가 제대로 할당되지 않았고, 또한 그 client에서 intent처리가 되지 않음.

--- app/src/main/java/com/almomin/skatemap/MainActivity.java
+++ app/src/main/java/com/almomin/skatemap/MainActivity.java
@@ -2,7 +2,9 @@ package com.almomin.skatemap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -18,20 +20,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mywebView=(WebView) findViewById(R.id.webview);
-        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.setWebViewClient(new MyWebViewClient());
         mywebView.getSettings().setTextZoom(100);
         WebSettings webSettings=mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mywebView.loadUrl("https://skatemap.kr/");
     }
-    public class mywebClient extends WebViewClient {
+    public class MyWebViewClient extends WebViewClient {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view,url,favicon);
         }
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
+            if ("www.example.com".equals(Uri.parse(url).getHost())) {
+                // This is my website, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
             return true;
         }
     }
